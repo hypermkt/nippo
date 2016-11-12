6ad5dfcaa3b271d27d15e483a45a36afd2ddf713
Makoto Chiba <hypermkt@paperboy.co.jp>
2016-11-12 16:56:23 +0900

    トップページを表示させる

---
+++ src/js/components/App.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    this is App.vue
+  </div>
+</template>
--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router';
+import App from './components/App.vue';
 
-new Vue({
-  el: '#app',
-  data: {
-    message: 'Hello'
-  }
+Vue.use(VueRouter);
+
+const routes = [
+  { path: '/', component: App }
+];
+
+const router = new VueRouter({
+  routes
 });
+
+new Vue({router}).$mount('#app');
