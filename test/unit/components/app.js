c7b42de1c993e183f4a8d31f21147c4db0635ef7
Makoto Chiba <hypermkt@paperboy.co.jp>
2016-11-19 18:35:56 +0900

    メソッド存在確認テストの調整

--- test/unit/components/app.js
+++ test/unit/components/app.js
@@ -1,11 +1,8 @@
 import App from '../../../src/js/components/app.vue';
 
 describe('Appコンポーネント', function() {
-  it('should have a created hook', function() {
+  it('メソッドが存在する', function() {
     expect(App.created).to.be.a('function');
-  });
-
-  it('should have a fetchNippoes method', function() {
     expect(App.methods.fetchNippoes).to.be.a('function');
   });
 });
